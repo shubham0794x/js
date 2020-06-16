df91b38197f6f9660dc087c9cf0a50b6a6d92428
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-06-16 17:05:05 +0530

    API protocol

--- src/component/withListLoading.js
+++ src/component/withListLoading.js
@@ -1,23 +1,17 @@
 import React from 'react';
 
 function WithListLoading(Component) {
-    return function WihLoadingComponent ({
-        isLoading,
-        ...props
-    }) {
-        if (!isLoading) return <Component {
-            ...props
-        }
-        />;
-        return ( <
-                p style = {
+    return function WihLoadingComponent ({ isLoading, ...props }) {
+        if (!isLoading) return <Component {...props}/>;
+        return ( 
+            < p style = {
                     {
                         textAlign: 'center',
                         fontSize: '30px'
                     }
                 } >
-                Hold on, fetching data may take some time: ) <
-            /p>
+                Hold on, fetching data may take some time: ) 
+                </p>
     );
 };
 }
